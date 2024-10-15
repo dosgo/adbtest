0db3c24c05ac0970f74eed60d989fbb86aa76c07
dosgo <dosgo@qq.com>
2024-10-15 19:09:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,17 +6,24 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"encoding/binary"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"math"
 	"math/big"
 	"net"
 	"os"
 	"time"
+
+	"golang.org/x/crypto/hkdf"
 
 	"salsa.debian.org/vasudev/gospake2"
 	"salsa.debian.org/vasudev/gospake2/ed25519group"
@@ -28,12 +35,13 @@ const (
 	serverName = "adb pair server\000"
 	info       = "adb pairing_auth aes-128-gcm key"
 	hkdfKeyLen = 16 // 128 bits
-	gcmIVLen   = 12
+
+	gcmIVLen = 12
 )
 
 type PairingAuthCtx struct {
 	spacke2Ctx *gospake2.SPAKE2
-	secretKey  [hkdfKeyLen]byte
+	secretKey  []byte
 	decIV      uint64
 	encIV      uint64
 }
@@ -63,7 +71,19 @@ func (p *PairingAuthCtx) ProcessMsg(theirMsg []byte) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	copy(p.secretKey[:], buf[:16])
+	var keyInfo = "adb pairing_auth aes-128-gcm key"
+
+	// 创建一个新的HKDF实例，使用SHA-256作为哈希函数
+	hkdfExtractor := hkdf.New(sha256.New, buf, nil, []byte(keyInfo))
+
+	p.secretKey = make([]byte, hkdfKeyLen)
+	// 生成密钥
+	if _, err := hkdfExtractor.Read(p.secretKey); err != nil {
+		fmt.Printf("Error generating key: %v\n", err)
+
+	}
+	fmt.Printf("p.secretKey[:]:%s\r\n", string(p.secretKey))
+
 	return true, nil
 }
 
@@ -86,6 +106,7 @@ func (p *PairingAuthCtx) Encrypt(in []byte) ([]byte, error) {
 }
 
 func (p *PairingAuthCtx) Decrypt(in []byte) ([]byte, error) {
+	fmt.Printf("p.secretKey[:]:%s\r\n", string(p.secretKey[:]))
 	block, err := aes.NewCipher(p.secretKey[:])
 	if err != nil {
 		return nil, err
@@ -110,6 +131,34 @@ func (p *PairingAuthCtx) Decrypt(in []byte) ([]byte, error) {
 		return nil, err
 	}
 	return plaintext, nil
+}
+
+func genPeerInfo(name string) []byte {
+	publicKey, _ := readRSAPublicKeyFromFile("ddd.pem")
+
+	ADB_RSA_PUB_KEY := byte(0)
+	var ANDROID_PUBKEY_ENCODED_SIZE = 524
+
+	// 计算pkeySize
+	pkeySize := 4 * int(math.Ceil(float64(ANDROID_PUBKEY_ENCODED_SIZE)/3.0))
+
+	// 创建一个足够大的缓冲区
+	buf := new(bytes.Buffer)
+	buf.Grow(pkeySize + len(name) + 2)
+
+	// 编码公钥
+	encodedPublicKey := base64.StdEncoding.EncodeToString(publicKey)
+	if _, err := buf.Write([]byte(encodedPublicKey)); err != nil {
+		return nil
+	}
+
+	// 获取并写入用户信息
+	userInfo := fmt.Sprintf(" %s\u0000", name)
+	if _, err := buf.Write([]byte(userInfo)); err != nil {
+		return nil
+	}
+	return append([]byte{ADB_RSA_PUB_KEY}, buf.Bytes()...)
+
 }
 
 func main() {
@@ -138,9 +187,9 @@ func main() {
 		InsecureSkipVerify: true,
 	}
 
-	var password = "675276"
+	var password = "923146"
 	// 使用TLS配置创建一个TCP连接
-	conn, err := tls.Dial("tcp", "192.168.78.137:36231", tlsConfig)
+	conn, err := tls.Dial("tcp", "172.30.17.16:40701", tlsConfig)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
@@ -163,38 +212,17 @@ func main() {
 	}
 	fmt.Printf("clientMsg:%v\r\n", clientMsg)
 
-	var sendBuf bytes.Buffer
-	version := byte(1)
-	msgType := byte(0)                       // 类型
-	payloadSize := int32(len(clientMsg[1:])) // 负载大小
-
-	// 使用binary.Write将字段按大端字节序写入缓冲区
-	if err := binary.Write(&sendBuf, binary.BigEndian, version); err != nil {
-		fmt.Printf("Error writing version: %v\n", err)
-		return
-	}
-	if err := binary.Write(&sendBuf, binary.BigEndian, msgType); err != nil {
-		fmt.Printf("Error writing type: %v\n", err)
-		return
-	}
-	if err := binary.Write(&sendBuf, binary.BigEndian, payloadSize); err != nil {
-		fmt.Printf("Error writing payload size: %v\n", err)
-		return
-	}
-	_, err = conn.Write(sendBuf.Bytes())
-	fmt.Printf("hand:%v\r\n", sendBuf.Bytes())
+	headerBuf := packetHeader(1, 0, uint32(len(clientMsg[1:])))
+	_, err = conn.Write(headerBuf)
+	fmt.Printf("hand:%v\r\n", headerBuf)
 	_, err = conn.Write(clientMsg[1:])
 	if err != nil {
 		log.Fatalf("Failed to write to device: %v", err)
 	}
 
-	// 接收并处理设备的消息
-	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		log.Fatalf("Failed to read from device: %v", err)
-	}
-
+	_, _, headerLen := readPacketHeader(conn)
+	fmt.Printf("headerLen:%d\r\n", headerLen)
+	buf := make([]byte, 1024*10)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatalf("Failed to read from device: %v", err)
@@ -214,16 +242,23 @@ func main() {
 	}
 
 	// 测试加密和解密
-	plaintext := []byte("Hello, ADB!")
-	ciphertext, err := alice.Encrypt(plaintext)
+	peerInfo := genPeerInfo("test")
+	fmt.Printf("peerInfo:%v\r\n", peerInfo)
+	ciphertext, err := alice.Encrypt(peerInfo)
 	if err != nil {
 		log.Fatalf("Failed to encrypt: %v", err)
 	}
+
+	peerInfoHead := packetHeader(1, 1, uint32(len(ciphertext)))
+	conn.Write(peerInfoHead)
+	fmt.Printf("peerInfoHead:%v\r\n", peerInfoHead)
 	_, err = conn.Write(ciphertext)
 	if err != nil {
 		log.Fatalf("Failed to write encrypted data: %v", err)
 	}
 
+	_, _, peerInfoLen := readPacketHeader(conn)
+	fmt.Printf("peerInfoLen:%d\r\n", peerInfoLen)
 	// 读取并解密设备的响应
 	n, err = conn.Read(buf)
 	if err != nil {
@@ -235,6 +270,50 @@ func main() {
 		log.Fatalf("Failed to decrypt: %v", err)
 	}
 	fmt.Printf("Decrypted response from device: %s\n", decrypted)
+}
+
+func readPacketHeader(r io.Reader) (byte, byte, uint32) {
+	var version byte
+	var msgType byte
+	var payloadSize uint32
+	// 读取版本号
+	if err := binary.Read(r, binary.BigEndian, &version); err != nil {
+		return version, msgType, payloadSize
+	}
+
+	// 读取消息类型
+	if err := binary.Read(r, binary.BigEndian, &msgType); err != nil {
+		return version, msgType, payloadSize
+	}
+
+	// 读取负载大小
+	if err := binary.Read(r, binary.BigEndian, &payloadSize); err != nil {
+		return version, msgType, payloadSize
+	}
+
+	return version, msgType, payloadSize
+}
+
+func packetHeader(version byte, msgType byte, payloadSize uint32) []byte {
+	var sendBuf bytes.Buffer
+	//version := byte(1)
+	//msgType := byte(0)                       // 类型
+	//payloadSize := int32(len(clientMsg[1:])) // 负载大小
+
+	// 使用binary.Write将字段按大端字节序写入缓冲区
+	if err := binary.Write(&sendBuf, binary.BigEndian, version); err != nil {
+		fmt.Printf("Error writing version: %v\n", err)
+		return nil
+	}
+	if err := binary.Write(&sendBuf, binary.BigEndian, msgType); err != nil {
+		fmt.Printf("Error writing type: %v\n", err)
+		return nil
+	}
+	if err := binary.Write(&sendBuf, binary.BigEndian, payloadSize); err != nil {
+		fmt.Printf("Error writing payload size: %v\n", err)
+		return nil
+	}
+	return sendBuf.Bytes()
 }
 
 func generateCert(certFile, keyFile string) error {
@@ -298,45 +377,36 @@ func fileExists(filename string) bool {
 	return !os.IsNotExist(err)
 }
 
-/*
-func  doExchangePeerInfo() {
-	// Encrypt PeerInfo
-	ByteBuffer buffer = ByteBuffer.allocate(PeerInfo.MAX_PEER_INFO_SIZE).order(ByteOrder.BIG_ENDIAN);
-	mPeerInfo.writeTo(buffer);
-	byte[] outBuffer = mPairingAuthCtx.encrypt(buffer.array());
-	if (outBuffer == null) {
-		Log.e(TAG, "Failed to encrypt peer info");
-		return false;
+func readRSAPublicKeyFromFile(filename string) ([]byte, error) {
+	// 读取文件内容
+	pemData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
-	// Write out the packet header
-	PairingPacketHeader ourHeader = createHeader(PairingPacketHeader.PEER_INFO, outBuffer.length);
-	// Write out the encrypted payload
-	writeHeader(ourHeader, outBuffer);
-
-	// Read in the peer's packet header
-	PairingPacketHeader theirHeader = readHeader();
-	if (theirHeader == null || !checkHeaderType(PairingPacketHeader.PEER_INFO, theirHeader.type)) return false;
-
-	// Read in the encrypted peer certificate
-	byte[] theirMsg = new byte[theirHeader.payloadSize];
-	mInputStream.readFully(theirMsg);
-
-	// Try to decrypt the certificate
-	byte[] decryptedMsg = mPairingAuthCtx.decrypt(theirMsg);
-	if (decryptedMsg == null) {
-		Log.e(TAG, "Unsupported payload while decrypting peer info.");
-		return false;
+	// 解析PEM块
+	block, _ := pem.Decode(pemData)
+	if block == nil || block.Type != "CERTIFICATE" {
+		return nil, errors.New("failed to decode PEM block containing the certificate")
 	}
 
-	// The decrypted message should contain the PeerInfo.
-	if (decryptedMsg.length != PeerInfo.MAX_PEER_INFO_SIZE) {
-		Log.e(TAG, "Got size=" + decryptedMsg.length + " PeerInfo.size=" + PeerInfo.MAX_PEER_INFO_SIZE);
-		return false;
+	// 解析证书
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse certificate: %w", err)
 	}
 
-	PeerInfo theirPeerInfo = PeerInfo.readFrom(ByteBuffer.wrap(decryptedMsg));
-	Log.d(TAG, theirPeerInfo.toString());
-	return true;
+	// 断言公钥类型为*rsa.PublicKey
+	rsaPub, ok := cert.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("the public key is not an RSA public key")
+	}
+
+	// 将公钥转换为DER编码的字节切片
+	derBytes, err := x509.MarshalPKIXPublicKey(rsaPub)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal public key: %w", err)
+	}
+
+	return derBytes, nil
 }
-*/

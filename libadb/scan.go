1d6dde4ff37695cd6409abf8c82fb466a5385422
dosgo <dosgo@qq.com>
2024-10-23 18:44:56 +0800

    Update scan.go

--- libadb/scan.go
+++ libadb/scan.go
@@ -1,10 +1,14 @@
 package libadb
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
-	"github.com/hashicorp/mdns"
+	"github.com/grandcat/zeroconf"
 	"github.com/oleksandr/bonjour"
 )
 
@@ -48,16 +52,38 @@ func StopScanAddr() {
 	resolver.Exit <- true
 }
 
-func DDD() {
-	// Make a channel for results and start listening
-	entriesCh := make(chan *mdns.ServiceEntry, 4)
-	go func() {
-		for entry := range entriesCh {
-			fmt.Printf("Got new entry: %v\n", entry)
-		}
-	}()
+func ScanTest() {
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		log.Fatalln("Failed to initialize resolver:", err.Error())
+	}
 
-	// Start the lookup
-	mdns.Lookup(fmt.Sprintf("_%s._tcp", SERVICE_TYPE_ADB), entriesCh)
+	entries := make(chan *zeroconf.ServiceEntry)
+	go scanRes(entries)
+
+	entries1 := make(chan *zeroconf.ServiceEntry)
+	go scanRes(entries1)
+
+	entries2 := make(chan *zeroconf.ServiceEntry)
+	go scanRes(entries2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	err = resolver.Browse(ctx, fmt.Sprintf("_%s._tcp", SERVICE_TYPE_ADB), "", entries)
+	err = resolver.Browse(ctx, fmt.Sprintf("_%s._tcp", SERVICE_TYPE_TLS_PAIRING), "", entries1)
+	err = resolver.Browse(ctx, fmt.Sprintf("_%s._tcp", SERVICE_TYPE_TLS_CONNECT), "", entries2)
+	if err != nil {
+		log.Fatalln("Failed to browse:", err.Error())
+	}
+	fmt.Println("按任意键继续...")
+	bufio.NewReader(os.Stdin).ReadByte()
+	<-ctx.Done()
 	//close(entriesCh)
+}
+
+func scanRes(results <-chan *zeroconf.ServiceEntry) {
+	for entry := range results {
+		log.Println(entry)
+	}
+	log.Println("No more entries.")
 }
